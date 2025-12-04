c52694b239f7eb0455f97900316f59627ec1e003
Ismail2805196 <shaikhismail000111@gmail.com>
2025-12-04 15:43:08 +0530

    AddEditDelete

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Button, ListGroup } from "react-bootstrap";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!task.trim()) return;
+    const updateList = [...list, task];
+    setList(updateList);
+    console.log("first", updateList);
+    setTask("");
+  };
+  const editTask = (index) => {
+    const update = prompt("Edit task", list[index]);
+    if (update && update.trim()) {
+      const updateList = [...list];
+      updateList[index] = update;
+      setList(updateList);
+      console.log("edit", updateList);
+    }
+  };
+  const deleteTask = (index) => {
+    // alert("delete");
+    confirm("are you sure?");
+    const updateList = list.filter((_, i) => i !== index);
+    setList(updateList);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app">
+        <h1>To Do App </h1>
+        <form>
+          <input
+            type="text"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
+          <Button variant="primary" onClick={handleSubmit}>
+            Add
+          </Button>
+         
+          <ListGroup className="mt-3">
+            {list.map((t, index) => (
+              <ListGroup.Item
+                key={index}
+                className="d-flex justify-content-between align-items-center"
+              >
+                {t}
+
+                <Button
+                  onClick={() => editTask(index)}
+                  style={{ backgroundColor: "green", color: "white" }}
+                >
+                  Edit
+                </Button>
+
+                <Button
+                  onClick={() => deleteTask(index)}
+                  style={{ backgroundColor: "red", color: "white" }}
+                >
+                  Delete
+                </Button>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
